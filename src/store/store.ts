8b2b055cd62b21ca6bf7a4087d6f03d13608ec97
Joan <joandv.gil@gmail.com>
2022-09-13 19:17:21 -0300

    Remove unnecessary package

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,13 @@
-import {combineReducers,configureStore} from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import {  charactersSlice } from "../slices/slices";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { charactersSlice } from "../slices/slices";
 
 const rootReducer = combineReducers({
-    characters: charactersSlice.reducer,
+  characters: charactersSlice.reducer,
 });
 
 export const store = configureStore({
-    reducer: rootReducer,
-    devTools: process.env.NODE_ENV !== "production",
-    middleware:[thunk]
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
