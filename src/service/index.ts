07a1f8c282f6608d7b0fdd764398bd1fffc8a166
Joan <joandv.gil@gmail.com>
2022-09-18 13:42:52 -0300

    Fix getCharacters

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,11 +4,12 @@
     return response.results;
 } */
 
-export const getCharacters = async ( page: string, name:string) => {
-    const response = (name.length>0) ? 
-    await fetch("https://rickandmortyapi.com/api/character/?" + new URLSearchParams([["page", page],["name", name]]))
-    .then(response =>response.json()) : 
-    await fetch("https://rickandmortyapi.com/api/character/?" + new URLSearchParams(page))
-    .then(response =>response.json());
-    return response.results;
-}
+export const getCharacters = async (page: string, name: string) => {
+  const response = await fetch(
+    "https://rickandmortyapi.com/api/character/?" +
+      new URLSearchParams({ page, name })
+  );
+
+  const asJson = await response.json();
+  return asJson.results;
+};
